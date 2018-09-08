cf3828f7c831a92604f769d919ac60b833f358bf
Tom Meehan <a>
2018-09-08 18:43:36 +0100

    router handles variable routes

--- framework.rb
+++ framework.rb
@@ -21,7 +21,6 @@ class App
 
     def get(route_spec, &block)
       @routes << Route.new(route_spec, block)
-      p @routes
     end
 
     def each(&block)
@@ -32,12 +31,24 @@ class App
 
   class Route < Struct.new(:route_spec, :block)
     def match(request)
-      p "PATH: #{request.path} >>>> #{route_spec}"
-      if request.path == route_spec
-        block.call
-      else
-        nil
+      path_components = request.path.split('/')
+      spec_components = route_spec.split('/')
+
+      params = {}
+
+      return nil unless path_components.length == spec_components.length
+
+      path_components.zip(spec_components).each do |path_comp, spec_comp|
+        is_var = spec_comp.start_with?(':')
+        if is_var
+          key = spec_comp.sub(/\A:/, '')
+          params[key] = path_comp
+        else
+          return nil unless path_comp = spec_comp
+        end
       end
+
+      block.call(params)
     end
   end
 end
